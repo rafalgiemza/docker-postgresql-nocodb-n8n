d4dc0c54fea24012324aa2ed2c7786816bea5797
Rafał Giemza <rafalgiemza@gmail.com>
2026-06-29 22:50:17 +0200

    initial commit

---
+++ docker/init-data.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e;
+
+
+if [ -n "${POSTGRES_NON_ROOT_USER:-}" ] && [ -n "${POSTGRES_NON_ROOT_PASSWORD:-}" ]; then
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+		CREATE USER ${POSTGRES_NON_ROOT_USER} WITH PASSWORD '${POSTGRES_NON_ROOT_PASSWORD}';
+		GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_NON_ROOT_USER};
+		GRANT CREATE ON SCHEMA public TO ${POSTGRES_NON_ROOT_USER};
+	EOSQL
+else
+	echo "SETUP INFO: No Environment variables given for the n8n user!"
+fi
+
+if [ -n "${NC_DB_USER:-}" ] && [ -n "${NC_DB_PASSWORD:-}" ] && [ -n "${NC_DB:-}" ]; then
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+		CREATE USER ${NC_DB_USER} WITH PASSWORD '${NC_DB_PASSWORD}';
+		CREATE DATABASE ${NC_DB} OWNER ${NC_DB_USER};
+	EOSQL
+else
+	echo "SETUP INFO: No Environment variables given for the NocoDB user!"
+fi
