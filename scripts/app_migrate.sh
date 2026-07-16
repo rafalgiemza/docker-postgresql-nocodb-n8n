3b695664894e0afef835f4a0f0b9ef56826540c5
Rafał Giemza <rafalgiemza@gmail.com>
2026-07-16 22:13:55 +0200

    fix app data schema

--- scripts/app_migrate.sh
+++ scripts/app_migrate.sh
@@ -39,6 +39,6 @@ echo "🚀 Run migrations for db ${APP_DB}..."
 # Możesz użyć dedykowanego narzędzia (np. Atlas, Prisma, Liquibase) lub czystego psql:
 docker exec -i docker-postgres-1 psql -v ON_ERROR_STOP=1 \
   --username "${APPDATA_OWNER_USER}" \
-  --dbname "${APP_DB}" < ./schema.sql
+  --dbname "${APP_DB}" < ./appdata/appdata_schema.sql
 
 echo "✅ Success."
